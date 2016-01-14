a5356179347b510b404cf285f353d9704e8aec9b
ShivaNambiar <shinam@shis-macbook-pro.local>
2016-01-14 11:28:11 -0500

    Initial Commit

---
+++ RPG-Game-OOP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RPG-Game-OOP
+//
+//  Created by Shi Nam on 2016-01-14.
+//  Copyright © 2016 Shi Nam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
